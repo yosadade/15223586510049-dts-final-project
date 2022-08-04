254d0d5b7826ceb94001a05f425ae1813de871a8
yosadade <masyosad@gmail.com>
2022-08-04 23:16:14 +0700

    protected route signup

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { onHandleSignUpWithEmailAndPassword } from "../../authentication/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../authentication/firebase";
+import { auth } from "../../authentication/firebase";
 import "./styles.css";
 
 const SignUp = () => {
